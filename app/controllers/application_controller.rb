e61656df3aa90b6f9e8acb560e76b94b4d000685
shoichi.kikuchi <bcaf.psvclluh.ahdb@gmail.com>
2019-01-30 05:47:31 +0000

    add login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  include SessionsHelper
+  
+  private
+  #ログインテスト用
+=begin
+  def login(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.authenticate(password)
+      session[:user_id] = @user.id
+      return true
+    else
+      return false
+    end
+  end
+=end
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,12 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:show, :new, :create, :edit, :update, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   def index
-    @tasks = Task.all.page(params[:page])
+    if logged_in?
+      # ログインしている場合
+      @tasks = current_user.tasks.all.page(params[:page])
+    end
   end
   
   def show
@@ -12,12 +17,13 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Task が正常に登録されました'
       redirect_to @task
     else
+      @tasks = current_user.tasks.all.page(params[:page])
       flash[:success] = 'Task が登録されませんでした'
       render :new
     end
@@ -52,5 +58,12 @@ class TasksController < ApplicationController
   #Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless  @task
+      redirect_to root_url
+    end
   end
 end
